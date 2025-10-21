2f339486f48e4e48c071510fa628dd2fa8f282c2
Luis Dionicio <ldionicio8888@gmail.com>
2025-10-21 10:53:29 -0500

    Calcular si es mayor de edad v2

--- Cerotercercuadro/src/main/java/FechaNacimiento/MayorEdad.java
+++ Cerotercercuadro/src/main/java/FechaNacimiento/MayorEdad.java
@@ -2,33 +2,29 @@ package FechaNacimiento;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class MayorEdad {
 
     public static void main(String[] args) {
-        
-        Scanner sc=new Scanner(System.in);
-        
+
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Ingrese fecha de nacimiento");
-        System.out.println("Año");
-        int year =sc.nextInt();
-        System.out.println("Mes");
-        int mes=sc.nextInt();
-        System.out.println("Dia");
-        int dia=sc.nextInt();
-        
-        LocalDate fechaNacimiento=LocalDate.of(year, mes, dia);
-        
-        int edad=Period.between(fechaNacimiento, LocalDate.now()).getYears();
-        
-        if(edad>=18){
+        String fecha = sc.nextLine();
+
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate fechaNacimiento = LocalDate.parse(fecha, formato);
+
+        int edad = Period.between(fechaNacimiento, LocalDate.now()).getYears();
+
+        if (edad >= 18) {
             System.out.println("Es mayor de edad");
-        }else{
+        } else {
             System.out.println("Es menor de edad");
         }
-        
-        
+
     }
 
 }
